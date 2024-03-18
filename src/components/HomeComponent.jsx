4274aa843818c06495a147c6974a1663c9e6c582
AdityaKannur <adityakannur28@gmail.com>
2024-03-18 10:07:43 +0530

    quiz-two

---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomeComponent.css"
+
+export default function HomeComponent(){
+  return(
+    <div className="home-container">
+      <h2 className="home-heading">Quiz App</h2>
+      <button className="home-button">Play</button>
+    </div>
+  )
+}
